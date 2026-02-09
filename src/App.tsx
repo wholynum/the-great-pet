24f34807593ad1342b1fbb25f125ec4084b5df59
wholynum <yakushi1000@gmail.com>
2026-02-09 16:57:03 +0700

    add routing, add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { Counter } from "./components/Counter";
-import './index.scss'
+import { Route, Routes } from "react-router-dom";
+import './styles/index.scss'
+import { Link } from "react-router-dom";
+import { AboutPageLazy } from "./pages/about/AboutPage.lazy";
+import { MainPageLazy } from "./pages/main/MainPage.lazy";
+import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
+
 const App = () => {
+
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
-            hello
-            <Counter/>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme} >Toggle theme</button>
+            <Link to={"/"}>Главная</Link>
+            <Link to={"/about"}>О сайте</Link>
+            <Suspense fallback={<div>loading</div>}>
+                <Routes>
+                    <Route path="/about" element={ <AboutPageLazy/> } />
+                    <Route path="/" element={ <MainPageLazy/> } />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
