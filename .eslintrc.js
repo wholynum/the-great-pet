97341d19aaa6f307a9a2266af0d06056fba64a2b
wholynum <yakushi1000@gmail.com>
2026-02-13 19:43:02 +0700

    add login state mangment

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,14 +49,12 @@ module.exports = {
                 ignoreAttribute: ['data-testid', 'to'],
             },
         ],
-        'max-len': ['error', { ignoreComments: true, code: 100 }],
+        'max-len': ['error', { ignoreComments: true, code: 120 }],
         'jsx-a11y/no-static-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
         'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
         'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies,
         'no-param-reassign': 'off',
-        // 'linebreak-style': 'off',
-        // semi: ['error', 'never'],
     },
     globals: {
         __IS_DEV__: true,
